0c9b1e6abf904b3fc5d7fb3870322bf218a4b970
Hossain Saddam <hossaincrr7@gmail.com>
2020-11-13 05:18:11 +0600

    add creative block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,4 @@
-import Swiper, {
-  Navigation,
-  Pagination
-} from 'swiper';
-// configure Swiper to use modules
+import { Swiper, Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 
 const jobCardSlider = new Swiper('.block-job__slider', {
@@ -17,4 +13,47 @@ const jobCardSlider = new Swiper('.block-job__slider', {
   wrapperClass: 'block-job__slider-container',
   slideClass: 'block-job__slider-item',
   slideActiveClass: 'is-active',
+})
+
+let interleaveOffset = 0.5;
+const creativeSlider = new Swiper('.block-creative__slider', {
+  loop: true,
+  speed: 1000,
+  watchSlidesProgress: true,
+  wrapperClass: 'block-creative__slider-container',
+  slideClass: 'block-creative__slider-item',
+  slideActiveClass: 'is-active',
+  navigation: {
+    nextEl: ".block-creative__slider-arrow--next",
+    prevEl: ".block-creative__slider-arrow--prev"
+  },
+  on: {
+
+    progress: function () {
+      let swiper = this;
+      for (let i = 0; i < swiper.slides.length; i++) {
+        let slideProgress = swiper.slides[i].progress,
+          innerOffset = swiper.width * interleaveOffset,
+          innerTranslate = slideProgress * innerOffset;
+
+        swiper.slides[i].querySelector(".block-creative__slider-parallax")
+          .style.transform =
+          "translateX(" + innerTranslate + "px)";
+      }
+    },
+    touchStart: function () {
+      let swiper = this;
+      for (let i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = "";
+      }
+    },
+    setTransition: function (swiper, speed) {
+      for (let i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = speed + "ms";
+        swiper.slides[i].querySelector(".block-creative__slider-parallax")
+          .style.transition =
+          speed + "ms";
+      }
+    }
+  }
 })
